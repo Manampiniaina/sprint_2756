8292947f301f8cc4b300f5a9315adea53230e528
Manampiniaina <155744150+Manampiniaina@users.noreply.github.com>
2025-04-29 20:31:42 +0300

    changes folder

--- src/com/sprint/exception/SprintException.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.exception;
-
-public class SprintException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public SprintException(String message) {
-        super(message);  
-    }
-}
